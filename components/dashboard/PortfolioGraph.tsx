74e2cc2b01de63f9824f40b59e997b23bc7ceefa
Nils-henrik <nilshenrikkarlsson92@hotmail.se>
2026-08-07 14:17:41 +0200

    chore: remove legacy dashboard portfolio graph

--- components/dashboard/PortfolioGraph.tsx
+++
@@ -1,5 +0,0 @@
-import DividendChart from "@/components/charts/DividendChart";
-
-export default function PortfolioGraph() {
-  return <DividendChart className="mt-0" chartHeightClassName="h-[165px]" />;
-}
