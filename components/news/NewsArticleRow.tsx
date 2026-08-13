cc85530102e7e84fcdfb04e7563b5caa36742af4
Nils-henrik <nilshenrikkarlsson92@hotmail.se>
2026-08-13 13:35:12 +0200

    Ta bort bild-overlay från nyhetsrader

--- components/news/NewsArticleRow.tsx
+++ components/news/NewsArticleRow.tsx
@@ -55,7 +55,6 @@ export default function NewsArticleRow({
             variant="row"
             objectPosition={article.thumbnailObjectPosition}
             mobileObjectPosition={article.mobileThumbnailObjectPosition}
-            showBrandOverlay={article.showImageBrandOverlay}
           />
         )}
 
