918b00d4cd9cc8f04fa3e3e328d64247ebefab21
Nils-henrik <nilshenrikkarlsson92@hotmail.se>
2026-08-24 19:45:57 +0200

    Align dashboard ticker spacing with compact height

--- app/dashboard/template.tsx
+++ app/dashboard/template.tsx
@@ -15,10 +15,10 @@ export default function DashboardTemplate({ children }: Props) {
     <>
       {showTicker && (
         <>
-          <div className="absolute left-20 right-0 top-24 z-20 hidden h-[60px] overflow-hidden lg:block xl:right-72">
+          <div className="absolute left-20 right-0 top-24 z-20 hidden h-[48px] overflow-hidden lg:block xl:right-72">
             <MarketTickerTape />
           </div>
-          <div aria-hidden="true" className="hidden h-[74px] lg:block" />
+          <div aria-hidden="true" className="hidden h-[62px] lg:block" />
           <div className="mb-8 lg:hidden">
             <MarketTickerTape />
           </div>
