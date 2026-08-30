9fc1f3a4b8ea1e0dd05a50f1c31ab1ad26f58150
Nils-henrik <nilshenrikkarlsson92@hotmail.se>
2026-08-30 14:08:05 +0200

    Publish Börsvecka 36 article

--- lib/news/get-articles.ts
+++ lib/news/get-articles.ts
@@ -1,3 +1,4 @@
+import { BORSVECKA_36_2026_JOBBRAPPORT_BROADCOM_EUROINFLATION_ARTICLE } from "@/data/news-articles/borsvecka-36-2026-jobbrapport-broadcom-euroinflation";
 import { VECKAN_SOM_GATT_29_AUGUSTI_2026_SEO_ARTICLE } from "@/data/news-articles/veckan-som-gatt-29-augusti-2026-seo";
 import { BORSSVERIGE_28_AUGUSTI_2026_ARTICLE } from "@/data/news-articles/borssverige-28-augusti-2026";
 import { NORDEN_I_CENTRUM_28_AUGUSTI_2026_ARTICLE } from "@/data/news-articles/norden-i-centrum-28-augusti-2026";
@@ -105,6 +106,7 @@ function resolveNewsArticleImages(article: NewsArticle): NewsArticle {
 }
 
 const PUBLISHED_NEWS_ARTICLES: NewsArticle[] = [
+  applyNewsSearchSeo(BORSVECKA_36_2026_JOBBRAPPORT_BROADCOM_EUROINFLATION_ARTICLE),
   applyNewsSearchSeo(VECKAN_SOM_GATT_29_AUGUSTI_2026_SEO_ARTICLE),
   applyNewsSearchSeo(BORSSVERIGE_28_AUGUSTI_2026_ARTICLE),
   applyNewsSearchSeo(NORDEN_I_CENTRUM_28_AUGUSTI_2026_ARTICLE),
