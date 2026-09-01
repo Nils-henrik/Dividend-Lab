c4ca5ed86603b163eb0fa8554f05c0e017cb4276
Nils-henrik <nilshenrikkarlsson92@hotmail.se>
2026-09-01 07:23:41 +0200

    Register BörsSverige for 1 September 2026

--- lib/news/get-articles.ts
+++ lib/news/get-articles.ts
@@ -1,3 +1,4 @@
+import { BORSSVERIGE_1_SEPTEMBER_2026_ARTICLE } from "@/data/news-articles/borssverige-1-september-2026";
 import { NORDEN_I_CENTRUM_1_SEPTEMBER_2026_ARTICLE } from "@/data/news-articles/norden-i-centrum-1-september-2026";
 import { BORSSVERIGE_31_AUGUSTI_2026_ARTICLE } from "@/data/news-articles/borssverige-31-augusti-2026";
 import { NORDEN_I_CENTRUM_31_AUGUSTI_2026_ARTICLE } from "@/data/news-articles/norden-i-centrum-31-augusti-2026";
@@ -109,6 +110,7 @@ function resolveNewsArticleImages(article: NewsArticle): NewsArticle {
 }
 
 const PUBLISHED_NEWS_ARTICLES: NewsArticle[] = [
+  applyNewsSearchSeo(BORSSVERIGE_1_SEPTEMBER_2026_ARTICLE),
   applyNewsSearchSeo(NORDEN_I_CENTRUM_1_SEPTEMBER_2026_ARTICLE),
   applyNewsSearchSeo(BORSSVERIGE_31_AUGUSTI_2026_ARTICLE),
   applyNewsSearchSeo(NORDEN_I_CENTRUM_31_AUGUSTI_2026_ARTICLE),
