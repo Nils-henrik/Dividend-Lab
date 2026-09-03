dbbda899934f2604acd79716529a939ce3290a4d
Nils-henrik <nilshenrikkarlsson92@hotmail.se>
2026-09-03 07:41:15 +0200

    Register Norden i centrum 3 September 2026

--- lib/news/get-articles.ts
+++ lib/news/get-articles.ts
@@ -1,3 +1,4 @@
+import { NORDEN_I_CENTRUM_3_SEPTEMBER_2026_ARTICLE } from "@/data/news-articles/norden-i-centrum-3-september-2026";
 import { BORSSVERIGE_2_SEPTEMBER_2026_ARTICLE } from "@/data/news-articles/borssverige-2-september-2026";
 import { NORDEN_I_CENTRUM_2_SEPTEMBER_2026_ARTICLE } from "@/data/news-articles/norden-i-centrum-2-september-2026";
 import { BORSSVERIGE_1_SEPTEMBER_2026_ARTICLE } from "@/data/news-articles/borssverige-1-september-2026";
@@ -112,6 +113,7 @@ function resolveNewsArticleImages(article: NewsArticle): NewsArticle {
 }
 
 const PUBLISHED_NEWS_ARTICLES: NewsArticle[] = [
+  applyNewsSearchSeo(NORDEN_I_CENTRUM_3_SEPTEMBER_2026_ARTICLE),
   applyNewsSearchSeo(BORSSVERIGE_2_SEPTEMBER_2026_ARTICLE),
   applyNewsSearchSeo(NORDEN_I_CENTRUM_2_SEPTEMBER_2026_ARTICLE),
   applyNewsSearchSeo(BORSSVERIGE_1_SEPTEMBER_2026_ARTICLE),
