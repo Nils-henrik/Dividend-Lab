f7c702f97a3e272dfab12af3b4e2e8e1ca9b0fff
Nils-henrik <nilshenrikkarlsson92@hotmail.se>
2026-09-12 22:04:37 +0200

    chore(news): export static image regression helpers

--- lib/news/images/index.ts
+++ lib/news/images/index.ts
@@ -1,6 +1,7 @@
 export * from "./company-logo-map";
 export * from "./render-series-image";
 export * from "./select-norden-companies";
+export * from "./static-regression";
 export * from "./templates";
 export * from "./types";
 export * from "./validate-generated-image";
